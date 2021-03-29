c1d3e7c1a201329adb3c200667a06916c1eb76f4
Daniel de Carvalho <ganimerd@gmail.com>
2021-03-29 14:19:14 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-print('hi')
-print('hi')
-print('hi')
+import requests
+
+URL_ALL = 'https://restcountries.eu/rest/v2/all'
+
+respota = requests.get(URL_ALL)
+
+
+print(respota)
+'''a respota e um objeto do <class 'requests.models.Response'>
+ print(type(resposta))
+ se eu der um resposta.text ele vai puxar as informacoes de todos os paises
+ 
+ '''
 
 '''
 1:34:10
